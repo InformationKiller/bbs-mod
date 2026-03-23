7471c99617f8fcf43a70c915854cf333030d919d
InformationKiller <121000471+InformationKiller@users.noreply.github.com>
2026-03-24 00:30:45 +0800

    sneaking pose

--- src/client/java/mchorse/bbs_mod/utils/pose/PoseManager.java
+++ src/client/java/mchorse/bbs_mod/utils/pose/PoseManager.java
@@ -1,7 +1,14 @@
 package mchorse.bbs_mod.utils.pose;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import mchorse.bbs_mod.BBSMod;
 import mchorse.bbs_mod.cubic.model.ModelManager;
+import mchorse.bbs_mod.data.DataToString;
+import mchorse.bbs_mod.data.types.MapType;
 import mchorse.bbs_mod.resources.Link;
+import mchorse.bbs_mod.utils.IOUtils;
 import mchorse.bbs_mod.utils.presets.DataManager;
 
 public class PoseManager extends DataManager
@@ -12,5 +19,32 @@ public class PoseManager extends DataManager
     protected Link getFile(String group)
     {
         return Link.assets(ModelManager.MODELS_PREFIX + group + "/poses.json");
+    }
+
+    protected Link getConfigFile(String group)
+    {
+        return Link.assets(ModelManager.MODELS_PREFIX + group + "/config.json");
+    }
+
+    @Override
+    public MapType getData(String group) {
+        MapType map = super.getData(group);
+
+        if (!map.has("default_sneaking"))
+        {
+            try (InputStream stream = BBSMod.getProvider().getAsset(getConfigFile(group)))
+            {
+                MapType configData = DataToString.mapFromString(IOUtils.readText(stream));
+                map.elements.put("default_sneaking", configData.get("sneaking_pose"));
+            }
+            catch (FileNotFoundException e)
+            {}
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return map;
     }
 }
