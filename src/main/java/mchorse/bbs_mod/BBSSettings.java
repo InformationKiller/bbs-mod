4d4ea06a93f1e282278f0dc084e63feff7b9d561
InformationKiller <121000471+InformationKiller@users.noreply.github.com>
2026-06-25 04:02:18 +0800

    Revert axes_scale

--- src/main/java/mchorse/bbs_mod/BBSSettings.java
+++ src/main/java/mchorse/bbs_mod/BBSSettings.java
@@ -409,7 +409,7 @@ public class BBSSettings {
 
 		builder.category("transformation", Icons.SCALE);
 		gizmos = builder.getBoolean("gizmos", true);
-		axesScale = builder.getFloat("axes_scale", 1F, 0F, 10F);
+		axesScale = builder.getFloat("axes_scale", 3F, 0F, 10F);
 		axesThickness = builder.getFloat("axes_thickness", 0.5F, 0.25F, 3F);
 		axesKeepScreenSize = builder.getBoolean("axes_keep_screen_size", true);
 		rotate3dSphere = builder.getBoolean("rotate_3d_sphere", true);
